b2952e028f7934dd6bf4e97e3537b51fdce9cca8
Analia Mok <analiam319@gmail.com>
2023-10-13 01:08:49 +0000

    test client only components

--- apps/test-app/app/routes/index.tsx
+++ apps/test-app/app/routes/index.tsx
@@ -1,15 +1,40 @@
 import { ThemeProvider, DEFAULT_THEME } from '@zendeskgarden/react-theming'
-import { LG, Blockquote, Code } from '@zendeskgarden/react-typography'
+import { LG, XXL, Blockquote, Code } from '@zendeskgarden/react-typography'
 import CustomThemedBlock from 'apps/test-app/components/zendesk/CustomThemedBlock'
+import { Combobox, Field, Label, Option } from '@zendeskgarden/react-dropdowns.next'
+import { ClientOnly } from 'remix-utils'
+import { json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { Skeleton } from '@zendeskgarden/react-loaders'
+
+export const loader = async () => {
+  return json({
+    items: ['apple', 'banana', 'orange'],
+  })
+}
 
 export default function Index() {
+  const { items } = useLoaderData<typeof loader>()
+
   return (
     <ThemeProvider theme={DEFAULT_THEME}>
-      {/* <Text>Hello World</Text> */}
       <LG>Hi</LG>
       <Blockquote>I am text</Blockquote>
       <CustomThemedBlock>Custom Component</CustomThemedBlock>
       <Code hue="green">Test Code</Code>
+
+      <ClientOnly fallback={<XXL><Skeleton /></XXL>}>
+        {() => (
+          <Field>
+            <Label>Choose a fruit</Label>
+            <Combobox isEditable={false}>
+              {items.map((item, index) => (
+                <Option value={item} key={item} isSelected={index === 0} />
+              ))}
+            </Combobox>
+          </Field>
+        )}
+      </ClientOnly>
     </ThemeProvider>
   )
 }
